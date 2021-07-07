0840b6f0314da86c5f91bb5f468b2d9572666c30
Dongwook Kwon <tomiyadragon@gmail.com>
2021-07-08 00:57:54 +0900

    Create get_api.py

---
+++ ocr_part/naver_cloud_ocr_api/get_api.py
@@ -0,0 +1,24 @@
+import json
+import base64
+import requests
+
+X-OCR-SECRET = "ZlBWaUFqdFp4bVFZTUpnTHZucGVwZFVOZUt3cllVY0o="
+OCR Invoke URL = "https://f04b1d88f83e41a6b1df8ce399b61fb5.apigw.ntruss.com/custom/v1/9782/eed9df9be97433637c2950146e96ff0956759904a38b1e5e6a9a69a7f6691a68/general"
+
+headers = {
+    "Content-Type" : "application/json"
+    "X-OCR-SECRET" : X-OCR-SECRET
+    }
+
+data = {
+        "version" : "",
+        "requetId" : "",
+        "timestamp" : 0
+        "images" : [
+            {
+            "name" : ""
+            "format" : ""
+            "data" : None
+                }
+            ]
+        }
