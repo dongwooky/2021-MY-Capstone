8dded7ff7995fd97b4f9ab58567e958324468ddc
Dongwook Kwon <tomiyadragon@gmail.com>
2021-07-08 01:48:32 +0900

    modify get_api.py

--- ocr_part/naver_cloud_ocr_api/get_api.py
+++ ocr_part/naver_cloud_ocr_api/get_api.py
@@ -1,9 +1,15 @@
+import cv2
+
 import json
 import base64
 import requests
 
-X-OCR-SECRET = "ZlBWaUFqdFp4bVFZTUpnTHZucGVwZFVOZUt3cllVY0o="
-OCR Invoke URL = "https://f04b1d88f83e41a6b1df8ce399b61fb5.apigw.ntruss.com/custom/v1/9782/eed9df9be97433637c2950146e96ff0956759904a38b1e5e6a9a69a7f6691a68/general"
+count = 1
+
+image = cv2.imread("namecard.jpg")
+
+x_ocr_secret = "ZlBWaUFqdFp4bVFZTUpnTHZucGVwZFVOZUt3cllVY0o="
+ocr_invoke_url = "https://f04b1d88f83e41a6b1df8ce399b61fb5.apigw.ntruss.com/custom/v1/9782/eed9df9be97433637c2950146e96ff0956759904a38b1e5e6a9a69a7f6691a68/general"
 
 headers = {
     "Content-Type" : "application/json"
@@ -11,14 +17,12 @@ headers = {
     }
 
 data = {
-        "version" : "",
-        "requetId" : "",
-        "timestamp" : 0
         "images" : [
             {
-            "name" : ""
-            "format" : ""
-            "data" : None
+                "format" : "png"
+                "name" : "sample_image_{}".format(count),
+                "data" : null,
+                "url" : 
                 }
             ]
         }
