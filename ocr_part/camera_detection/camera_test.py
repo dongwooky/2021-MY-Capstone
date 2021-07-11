ae1c1e891ad500b131242fd9f1ed8c6e5fdbbbf7
Dongwook Kwon <tomiyadragon@gmail.com>
2021-07-11 20:48:18 +0900

    Update camera_test.py

--- ocr_part/camera_detection/camera_test.py
+++ ocr_part/camera_detection/camera_test.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import sys
 
 def on_trackbar(pos, frame_unnoise):
@@ -12,6 +13,22 @@ def on_trackbar(pos, frame_unnoise):
                                 cv2.THRESH_BINARY, bsize, 5)
     
     cv2.imshow('dst', dst)
+    
+def local_binary(frame, block_number):
+    block_frame = np.zeros(frame.shape, np.uint8)
+    
+    block_width = frame.shape[1] // block_number
+    block_height = frame.shape[0] // block_number
+    
+    for y in range(block_number):
+        for x in range(block_number):
+            frame_ = frame[y*block_height:(y+1)*block_height, x*block_width:(x+1)*block_width]
+            block_frame_ = block_frame[y*block_height:(y+1)*block_height, x*block_width:(x+1)*block_width]
+            cv2.threshold(frame_, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU, block_frame_)
+            
+    return block_frame
+    
+    
 
 capture = cv2.VideoCapture(1)
 
@@ -28,7 +45,8 @@ while True:
         
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame_unnoise = cv2.bilateralFilter(frame_gray, -1, 10, 5)
-    _, frame_binary = cv2.threshold(frame_unnoise, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+#    _, frame_binary = cv2.threshold(frame_unnoise, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    frame_binary = local_binary(frame_unnoise, 4)
     contours, _ = cv2.findContours(frame_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
     for points in contours:
@@ -45,10 +63,8 @@ while True:
     cv2.imshow('frame', frame)
     cv2.imshow('frame_unnoise', frame_unnoise)
     cv2.imshow('frame_binary', frame_binary)
-    
-    cv2.namedWindow('dst')
-    cv2.createTrackbar('Block size', 'dst', 0, 200, on_trackbar(frame_unnoise))
-    cv2.setTrackbarPos('Block size', 'dst', 11)
+#    cv2.imshow('frame_local_binary', local_binary(frame_unnoise, 4))
+
     
     if cv2.waitKey(33) == ord('q'):
         break
